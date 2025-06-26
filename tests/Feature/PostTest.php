b6e172d8b8e046863eff59cbbe5d58d7cd1e66aa
Mirco <mirco.pieper92@gmail.com>
2025-06-26 13:49:11 +0200

    test: added test coverage for creating a post

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,15 +4,63 @@
 use App\Http\Requests\CreatePostRequest;
 use App\Models\User;
 
-it('validates post request', function () {
-    $postRequest = new CreatePostRequest();
-    $postRequest->merge([
-        'body' => 'This is my first Post',
-    ]);
-    $postRequest->validate();
-
-
+beforeEach(function () {
+   User::factory()->create();
 });
+
+it('passes validation with valid data', function () {
+    $data = [
+        'user_id' => 1,
+        'title' => 'Hello World',
+        'body' => 'This is my first post!',
+    ];
+
+    $request = new CreatePostRequest();
+    $validator = Validator::make($data, $request->rules());
+
+    expect($validator->passes())->toBeTrue();
+});
+
+it('dont passes validation when user is not set', function () {
+    $data = [
+        'title' => 'Hello World',
+        'body' => 'This is my first post!',
+    ];
+
+    $request = new CreatePostRequest();
+    $validator = Validator::make($data, $request->rules());
+
+    expect($validator->fails())->toBeTrue();
+    expect($validator->errors()->has('user_id'))->toBeTrue();
+});
+
+it('dont passes validation when title is not set', function () {
+    $data = [
+        'user_id' => 1,
+        'body' => 'This is my first post!',
+    ];
+
+    $request = new CreatePostRequest();
+    $validator = Validator::make($data, $request->rules());
+
+    expect($validator->fails())->toBeTrue();
+    expect($validator->errors()->has('title'))->toBeTrue();
+});
+
+it('dont passes when user is not in database', function () {
+    $data = [
+        'user_id' => 9999,
+        'title' => 'Hello World',
+        'body' => 'This is my first post!',
+    ];
+
+    $request = new CreatePostRequest();
+    $validator = Validator::make($data, $request->rules());
+
+    expect($validator->fails())->toBeTrue();
+    expect($validator->errors()->has('user_id'))->toBeTrue();
+});
+
 it('can create post', function () {
     User::factory()->create();
     $postRequest = new CreatePostRequest();
